85cf1bde0d753bf8c9c8f9f74ffaaa377f3b262b
Chingpo-Lin <m13823601165@163.com>
2022-12-22 12:30:44 -0800

    Added get coupon

---
+++ xd-common/src/main/java/org/example/enums/CouponStateEnum.java
@@ -0,0 +1,19 @@
+package org.example.enums;
+
+public enum CouponStateEnum {
+
+    /**
+     * new coupon can be used
+     */
+    NEW,
+
+    /**
+     * used coupon
+     */
+    USED,
+
+    /**
+     * expired coupon
+     */
+    EXPIRED;
+}
