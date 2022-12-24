8d24c6e9130bf91c12a89e0a6ef0104685978982
Chingpo-Lin <m13823601165@163.com>
2022-12-24 15:18:44 -0800

    Added product detail and pagination

--- xd-product-service/src/main/java/org/example/service/ProductService.java
+++ xd-product-service/src/main/java/org/example/service/ProductService.java
@@ -2,6 +2,9 @@ package org.example.service;
 
 import org.example.model.ProductDO;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.example.vo.ProductVO;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -11,6 +14,20 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @author Bob
  * @since 2022-12-24
  */
-public interface ProductService extends IService<ProductDO> {
+public interface ProductService {
 
+    /**
+     * paging product list
+     * @param page
+     * @param size
+     * @return
+     */
+    Map<String, Object> page(int page, int size);
+
+    /**
+     * find product detail by id
+     * @param productId
+     * @return
+     */
+    ProductVO findProductById(long productId);
 }
