59f3753276847164337b933de7ff13fbd3752da6
Chingpo-Lin <m13823601165@163.com>
2022-12-25 14:07:42 -0800

    Added nacos and feign

--- xd-order-service/src/main/java/org/example/service/impl/ProductOrderServiceImpl.java
+++ xd-order-service/src/main/java/org/example/service/impl/ProductOrderServiceImpl.java
@@ -19,6 +19,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductOrderServiceImpl implements ProductOrderService {
 
+    /**
+     * 1. check if submit order redundant
+     * 2. check address belongs to current user
+     * 3. get latest latest cart item and price
+     * 4. order check (if price same as latest)
+     *      - get coupon
+     *      - check price
+     * 5. lock coupon
+     * 6. lock stock
+     * 7. create order object
+     * 8. create child order object
+     * 9. send delay msg for auto close order
+     * 10 create payment info (use payment api)
+     * @param confirmOrderRequest
+     * @return
+     */
     @Override
     public JsonData confirmOrder(ConfirmOrderRequest confirmOrderRequest) {
         return null;
